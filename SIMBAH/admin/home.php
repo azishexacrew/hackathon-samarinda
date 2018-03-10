e1ae52f514a075e07fd092eb9847c5667fdf870f
ENVYUS1 <37231819+ENVYUS1@users.noreply.github.com>
2018-03-10 21:41:31 +0700

    add index

---
+++ SIMBAH/admin/home.php
@@ -0,0 +1,42 @@
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h2>Sales Statistics <small>Vestibulum purus quam scelerisque, mollis nonummy metus</small></h2>
+
+            <ul class="actions">
+                <li>
+                    <a href="#">
+                        <i class="zmdi zmdi-check-all"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="zmdi zmdi-trending-up"></i>
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" data-toggle="dropdown">
+                        <i class="zmdi zmdi-more-vert"></i>
+                    </a>
+
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li>
+                            <a href="#">Change Date Range</a>
+                        </li>
+                        <li>
+                            <a href="#">Change Graph Type</a>
+                        </li>
+                        <li>
+                            <a href="#">Other Settings</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+        <div class="card-body">
+            <div id="map" style="height: 400px; width: 100%;" class="flot-chart "></div>
+        </div>
+    </div>
+</div>

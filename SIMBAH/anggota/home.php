5e382244576a14e4361f8741642fa5298c76fd40
ENVYUS1 <37231819+ENVYUS1@users.noreply.github.com>
2018-03-10 23:09:14 +0700

    add index

---
+++ SIMBAH/anggota/home.php
@@ -0,0 +1,25 @@
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h2>TABUNGAN</h2>
+        </div>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Tanggal</th>
+                        <th>Sampah (KG)</th>
+                        <th>Setoran</th>               
+                        <th>Saldo</th>
+                    </tr>
+                </thead>
+                <tbody id="get-tabungan">
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>

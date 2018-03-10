a20352b31052b65e15b02de60c7421bf13b89c28
haidi20 <nhaidii@yahoo.com>
2018-03-10 20:08:26 +0800

    berhasil-upload-foto

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,9 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event ;
+
 class EventController extends Controller
 {
     public function store(){
-      return request()->file('gambar');
+      $event = new Event ;
+
+      $event->nama = request('nama');
+      $event->penjelasan = request('penjelasan');
+      $event->alamat = request('alamat');
+      $event->bentuk_tenant = request('bentuk_tenant');
+      $event->rekening = request('rekening');
+      $event->nama_rekening = request('nama_rekening');
+
+      if (request()->file('gambar')) {
+        @unlink(public_path('image/' . $event->foto));
+          $file           = request()->file('gambar');
+          $extension      = $file->getClientOriginalExtension();
+          $fileName       = str_random(8) . '.' . $extension;
+          request()->file('gambar')->move("image/", $fileName);
+          $event->gambar    = $fileName;
+      }
+
+      $event->save();
+
+      if ($event) {
+          return 'berhasil';
+      }else{
+          return 'gagal';
+      }
     }
 }

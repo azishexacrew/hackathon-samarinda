73a77bdaf3032913bf8b4284056d6e3b673b3304
ilham15 <ilhamshinohara@gmail.com>
2018-03-10 13:42:34 +0800

    dsfasd

--- resources/views/_layouts/basic.blade.php
+++ resources/views/_layouts/basic.blade.php
@@ -4,10 +4,15 @@
     <meta charset="utf-8">
     <meta name="csrf-token">
     <link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/custom.css') }}">
     <title></title>
   </head>
   <body>
     @yield('konten')
-    <script type="text/javascript" source="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
   </body>
 </html>

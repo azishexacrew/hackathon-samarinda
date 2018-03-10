6b186f1304748c94ba15525f0e022456297c25ee
ilham15 <ilhamshinohara@gmail.com>
2018-03-10 21:15:23 +0800

    update

--- resources/views/_layouts/basic.blade.php
+++ resources/views/_layouts/basic.blade.php
@@ -11,11 +11,15 @@
     <title></title>
   </head>
   <body>
+    @yield('konten')
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="{{asset('js/tengger.js')}}"></script>
 <script src="{{asset('js/lightbox.js')}}"></script>
 <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
+
   </body>
 </html>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,28 +1,70 @@
 @extends('_layouts.default-register')
 
 @section('tubuh')
-<div class="container-fluid text-center">
+<div class="container-fluid">
 		<div class="row"> 
-			<form>
+			<form id="register-form">
 			<div class="panel panel-default">
-			  <div class="panel-heading">
+			  <div class="panel-heading text-center">
 			  <div class="h2">Buat Akun</div>
 			</div>
-			<div class="panel-body">	
-				<div class="form-group col-sm-4 col-sm-offset-4" id="user-icon">
-					<i class="fas fa-user fa-5x"></i>
+			<div class="panel-body">
+				<div class="row">
+					<div class="alert alert-dismissible alert-danger col-sm-offset-3 col-sm-6">
+					  <button type="button" class="close" data-dismiss="alert">&times;</button>
+					  <strong>Pendaftaran Gagal
+					</div>				
+					<div class="alert alert-dismissible alert-success col-sm-offset-3 col-sm-6">
+					  <button type="button" class="close" data-dismiss="alert">&times;</button>
+					  <strong>Pendaftaran Berhasil
+					</div>		
 				</div>
-			  	<div class="form-group col-sm-offset-4 col-sm-4">
+
+				<div class="row">
+					<div class="form-group col-sm-4 col-sm-offset-4 text-center" id="user-icon">
+					<i class="fas fa-user fa-5x"></i>
+					</div>
+					<div class="col-sm-6">	
+			  	<div class="form-group col-sm-offset-7 col-sm-5">
 					<label for="username">Username</label>
 					<input type="text" class="form-control" name="username" required="">
 				</div>
-			  	<div class="form-group col-sm-offset-4 col-sm-4">
-					<label for="password">Password</label>
-					<input type="password" class="form-control" name="password" required="">
+				<div class="form-group col-sm-offset-7 col-sm-5">
+					<label for="email">Email</label>
+					<input type="email" class="form-control" name="email" required="">
 				</div>
-				<div class="form-group col-sm-offset-4 col-sm-4">
-
-					<button type="submit" class="btn btn-success" name="submit"><i class="fas fa-check"></i>Submit</button>
+				<div class="form-group col-sm-offset-7 col-sm-5">
+					<label for="name">Nama</label>
+					<input type="text" class="form-control" name="name" required="">
+				</div>
+				<div class="form-group col-sm-offset-7 col-sm-5">
+					<label for="jk">Jenis Kelamin</label>
+					<select class="form-control">
+						<option value="pria">Pria</option>
+						<option value="wanita">Wanita</option>
+					</select>
+				</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="form-group col-sm-5">
+							<label for="phone">Phone</label>
+							<input type="text" class="form-control" name="phone" required="">
+						</div>
+						<div class="col-sm-8"></div>
+						<div class="form-group col-sm-5">
+							<label for="alamat">Alamat</label>
+							<input type="text" class="form-control" name="alamat" required="">
+						</div>
+						<div class="col-sm-8"></div>
+					  	<div class="form-group col-sm-5">
+							<label for="password">Password</label>
+							<input type="password" class="form-control" name="password" required="">
+						</div>
+						<div class="col-sm-8"></div>
+						<div class="form-group col-sm-5">
+							<button type="submit" class="btn btn-success" name="submit"><i class="fas fa-check"></i>Submit</button>
+						</div>						
+					</div>
 				</div>
 			</div>
 			</form>

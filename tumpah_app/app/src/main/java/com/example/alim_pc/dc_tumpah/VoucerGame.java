c398c9cd3eaaf5d76d66df2a339dfec67fb4b39d
Akishino1998 <ekopujianto48@gmail.com>
2018-03-11 11:01:13 +0800

    app

---
+++ tumpah_app/app/src/main/java/com/example/alim_pc/dc_tumpah/VoucerGame.java
@@ -0,0 +1,13 @@
+package com.example.alim_pc.dc_tumpah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoucerGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voucer_game);
+    }
+}

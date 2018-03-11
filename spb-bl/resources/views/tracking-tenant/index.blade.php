7a91cf46951ad5d2e01afe0563a37a724ce1c068
rezapadillah <mrezapadillah20@gmail.com>
2018-03-11 09:13:23 +0800

    fix layout

--- spb-bl/resources/views/tracking-tenant/index.blade.php
+++ spb-bl/resources/views/tracking-tenant/index.blade.php
@@ -27,33 +27,59 @@
         @if(!is_null($term))
 
         @forelse($sewa as $index => $item)
-          <div class="card text-white bg-info mb-3" style="max-width: 20rem;">
-            <div class="card-body">
-              <h4 class="card-title">Kode Sewa : <u>{{$item->kode}}</u> Ditemukan!</h4>
-            </div>
+          <div class="col-md-12">
+            <div class="alert alert-dismissible alert-info text-center">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <strong>Kode Sewa : <u>{{$item->kode}}</u> Ditemukan!</strong>
           </div>
 
-          <div class="col-md-6 text-center">
-            <a href="">
-            <div class="card text-white bg-info mb-3" style="max-width: 20rem;">
-              <div class="card-body">
-                <h1 class="card-title"><i class="fa fa-list-alt"></i></h1>
-                <h5 class="card-title">Info & Status</h5>
-              </div>
-            </div>
-          </a>
-          </div>
 
-        <div class="col-md-6 text-center">
-          <a href="">
-            <div class="card text-white bg-info mb-3" style="max-width: 20rem;">
-              <div class="card-body">
-                <h1 class="card-title"><i class="fa fa-money"></i></h1>
-                <h5 class="card-title">Pembayaran</h5>
+            <div class="row text-center">
+              <div class="col-sm-6">
+                <a href="" data-toggle="modal" data-target="#myModal">
+                  <div class="card text-white bg-info mb-3" >
+                    <div class="card-body">
+                      <h1 class="card-title"><i class="fa fa-list-alt"></i></h1>
+                      <h5 class="card-title">Info & Status</h5>
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div class="col-sm-6">
+                <a href="">
+                  <div class="card text-white bg-info mb-3" >
+                    <div class="card-body">
+                      <h1 class="card-title"><i class="fa fa-money"></i></h1>
+                      <h5 class="card-title">Pembayaran</h5>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
-          </a>
-        </div>
+
+            <div class="modal fade" id="myModal">
+              <div class="modal-dialog">
+                <div class="modal-content">
+
+                  <!-- Modal Header -->
+                  <div class="modal-header">
+                    <h4 class="modal-title">Info & Status</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>
+
+                  <!-- Modal body -->
+                  <div class="modal-body">
+                    Modal body..
+                  </div>
+
+                  <!-- Modal footer -->
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                  </div>
+
+                </div>
+              </div>
+            </div>
 
         @empty
 
